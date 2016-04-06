18aff4f7825ebe5cc3f0e73dc8f3ad28066b34ca
Jason Sjambar <realjase@gmail.com>
2016-04-06 10:34:59 +0200

    First base commit
    Files will be edited etc.

---
+++ App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Project3Web.Startup))]
+namespace Project3Web
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
