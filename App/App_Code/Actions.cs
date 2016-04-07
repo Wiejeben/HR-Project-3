4dde9659a20aae10e43c48dddbb635736a41c4bf
Jeroen Meijer <sonic.ex@hotmail.com>
2016-04-07 12:04:14 +0200

    finished actions

--- App/App_Code/Actions.cs
+++ App/App_Code/Actions.cs
@@ -3,13 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-/// <summary>
-/// Summary description for Actions
-/// </summary>
 public class Actions
 {
     public static List<Location> inRadius(List<Location> locations, Vector2 center, int radius)
     {
+        //unfinished
         return new List<Location>();
+    }
+    public static int getDistance(Location pos1, Location pos2) {
+        {
+            //unfinished
+            return 0;
+        }
     }
 }
