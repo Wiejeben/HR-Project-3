931f72429f257fbbdaf24de45c526b1bdb19017a
Jason Sjambar <realjase@gmail.com>
2016-04-07 14:49:12 +0200

    test data for house, added css for house & search.

--- App/Search.aspx
+++ App/Search.aspx
@@ -2,7 +2,18 @@
 
 <asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" Runat="Server">
     <div class="jumbotron banner">
-        <h1>Zoekresultaten voor de zoekterm: ''</h1>
+        <h1>Zoekresultaten voor de zoekterm: '<%= query %>'</h1>
+    </div>
+    <div class="row">
+        <% foreach (int result in results) { %>
+        <div class="col-md-12 search_result">
+            <a href="/house.aspx?hid=<%= result%>" class="search_container">
+                <h2>Lorem ipsum dolor sit amet... mooi maken etc..</h2>
+                <small>Lorem ipsum dolor sit amet.... extra tekst?</small>
+                <span class="glyphicon glyphicon-chevron-right"></span>
+            </a>
+        </div>
+        <% } %>
     </div>
 </asp:Content>
 
--- App/Search.aspx.cs
+++ App/Search.aspx.cs
@@ -7,8 +7,24 @@ using System.Web.UI.WebControls;
 
 public partial class Search : System.Web.UI.Page
 {
+    public string query;
+    public List<int> results = new List<int>(new int[]{ 1,2,3,4,5 });
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        // Hebben we een parameter ontvangen?
+        if (Request.QueryString != null && Request.QueryString.Count > 0)
+        {
+            // Is het wel de juiste parameter?
+            if (Request.QueryString["q"] != null)
+            {
+                query = Request.QueryString["q"];
+                // Hier doen we een query uitvoeren om te checken of het bestaat.
+            }
+        }
+        else
+        {
+            // Redirect implementation
+            // Server.Transfer("Default.aspx", true);
+        }
     }
 }
