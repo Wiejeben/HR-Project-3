e290dd0fdf90ee700319097abc171d1f5c25fa12
Marhek <vdheidenMark@gmail.com>
2016-04-07 12:45:10 +0200

    Import

---
+++ App/App_Code/Import.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+public interface Import
+{
+    string Contents { get; set; }
+    bool Get(string filename);
+    bool Set();
+    List<Import> Implements();
+}
