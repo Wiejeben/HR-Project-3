9cd214e54950a1ba833a586cb39864ad018ac578
Jeroen Meijer <sonic.ex@hotmail.com>
2016-04-07 11:39:52 +0200

    DataObject

---
+++ App/App_Code/DataObject.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for DataObject
+/// </summary>
+interface DataObject
+{
+    List<DataObject> All();
+    List<DataObject> Find(string query);
+    DataObject Get(int id);
+}
