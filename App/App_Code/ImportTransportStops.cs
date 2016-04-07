459cd74f65e5a05545fdf481cf4b182f159accb0
Marhek <vdheidenMark@gmail.com>
2016-04-07 13:06:10 +0200

    ImportTransportStops Update

--- App/App_Code/ImportTransportStops.cs
+++ App/App_Code/ImportTransportStops.cs
@@ -5,26 +5,26 @@ using System.Web;
 
 public class ImportTransportStops : Import
 {
-    public string Content { get; set; }
+    private string Content { get; set; }
 
         public ImportTransportStops(string content)
     {
         this.Content = content;
     }
 
-    public bool Get(string filename)
+    private bool Get(string filename)
     {
         //Unfinished
         return false;
     }
 
-    public bool Set()
+    private bool Set()
     {
         //Unfinished
         return false;
     }
 
-    public List<Import> Implements()
+    private List<Import> Implements()
     {
         //Unfinished
         return new List<Import>();
