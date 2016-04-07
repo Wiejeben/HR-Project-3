62a06a20652f786cefba9ad640f7946b2aa55cbf
Jeroen Meijer <sonic.ex@hotmail.com>
2016-04-07 11:45:20 +0200

    Vector2

--- App/App_Code/DataObject.cs
+++ App/App_Code/DataObject.cs
@@ -3,9 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-/// <summary>
-/// Summary description for DataObject
-/// </summary>
 interface DataObject
 {
     List<DataObject> All();
---
+++ App/App_Code/Vector2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Vector2
+/// </summary>
+public class Vector2
+{
+    double X;
+    double Y;
+    public Vector2(double posx, double posy)
+    {
+        this.X = posx;
+        this.Y = posy;
+    }
+}
