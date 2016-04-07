c2052cbaefac2bcf278ab7649f54a250e233acb6
Marhek <vdheidenMark@gmail.com>
2016-04-07 13:04:21 +0200

    TransportStops

--- App/App_Code/ImportTransportStops.cs
+++ App/App_Code/ImportTransportStops.cs
@@ -3,14 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-public class ImportTransportStops
+public class ImportTransportStops : Import
 {
-    private string Content { get; set; }
+    public string Content { get; set; }
 
-    public ImportTransportStops(string content)
+        public ImportTransportStops(string content)
     {
         this.Content = content;
     }
 
+    public bool Get(string filename)
+    {
+        //Unfinished
+        return false;
+    }
 
+    public bool Set()
+    {
+        //Unfinished
+        return false;
+    }
+
+    public List<Import> Implements()
+    {
+        //Unfinished
+        return new List<Import>();
+    }
 }
