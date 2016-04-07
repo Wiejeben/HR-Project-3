a12734f15456195555bd282e287f4dc56ff0ede8
Jeroen Meijer <sonic.ex@hotmail.com>
2016-04-07 12:16:03 +0200

    Street

---
+++ App/App_Code/Street.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Street
+/// </summary>
+public class Street : Location, DataObject
+{
+    public int ID;
+    public string Name, Intro, Content;
+    public Vector2 Pos { get; }
+    public bool Exists;
+
+    public Street(int id, string name, string intro, string content, Vector2 pos, bool exists)
+    {
+        this.ID = id;
+        this.Name = name;
+        this.Intro = intro;
+        this.Content = content;
+        this.Pos = pos;
+        this.Exists = exists;
+    }
+
+    public List<DataObject> All()
+    {
+        return new List<DataObject>();
+    }
+
+    public List<DataObject> Find(string query)
+    {
+        return new List<DataObject>();
+    }
+
+    public DataObject Get(int id)
+    {
+        throw new NotImplementedException();
+    }
+}
