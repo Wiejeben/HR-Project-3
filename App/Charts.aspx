36ecf7cb3c411854eb7b5053b358467586354552
Maarten de Graaf <wiejeben@gmail.com>
2016-04-08 17:54:53 +0200

    Code cleanup and fix navigatoin

--- App/Charts.aspx
+++ App/Charts.aspx
@@ -1,7 +1,9 @@
 ﻿<%@ Page Title="Charts" Language="C#" MasterPageFile="~/Main.Master" AutoEventWireup="true" CodeFile="Charts.aspx.cs" Inherits="Charts" %>
+
 <asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
     <h1>Charts</h1>
-    <div class="container chart">
+
+    <div class="chart">
         <canvas id="canvas" height="450" width="600"></canvas>
     </div>
 </asp:Content>
---
+++ App/assets/js/app.js
@@ -0,0 +1,56 @@
+﻿// Graph settings
+var randomScalingFactor = function () { return Math.round(Math.random() * 100) };
+
+var barChartData = {
+    labels: ["January", "February", "March", "April", "May", "June", "July"],
+    datasets: [
+        {
+            fillColor: "rgba(220,220,220,0.5)",
+            strokeColor: "rgba(220,220,220,0.8)",
+            highlightFill: "rgba(220,220,220,0.75)",
+            highlightStroke: "rgba(220,220,220,1)",
+            data: [randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor()]
+        },
+        {
+            fillColor: "rgba(151,187,205,0.5)",
+            strokeColor: "rgba(151,187,205,0.8)",
+            highlightFill: "rgba(151,187,205,0.75)",
+            highlightStroke: "rgba(151,187,205,1)",
+            data: [randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor()]
+        }
+    ]
+
+}
+
+$(document).ready(function () {
+
+    if ($('map').length != null)
+    {
+        // Google Maps
+        function initialize() {
+
+            var latlng = new google.maps.LatLng(35.7022077, 139.7722703);
+
+            var mapOptions = {
+                zoom: 8,
+                center: latlng
+            }
+
+            map = new google.maps.Map($('map'), mapOptions);
+
+            var marker = new google.maps.Marker({
+                position: latlng,
+                map: map,
+                title: 'Hey buddy.'
+            });
+        }
+
+        google.maps.event.addDomListener(window, 'load', initialize);
+    }
+
+    var ctx = $("canvas").get(0).getContext("2d");
+    window.myBar = new Chart(ctx).Bar(barChartData, {
+        responsive: false
+    });
+
+});
