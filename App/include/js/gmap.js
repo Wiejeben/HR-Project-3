a50e831f30f08d9954172accd32d52fc1f83cb69
Jason Sjambar <realjase@gmail.com>
2016-04-08 14:43:47 +0200

    japan focus + marker..

--- App/include/js/gmap.js
+++ App/include/js/gmap.js
@@ -1,11 +1,19 @@
 ﻿function initialize() {
-    geocoder = new google.maps.Geocoder();
-    var latlng = new google.maps.LatLng(-34.397, 150.644);
+
+    var latlng = new google.maps.LatLng(35.7022077, 139.7722703);
+
     var mapOptions = {
         zoom: 8,
         center: latlng
     }
+
     map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+    var marker = new google.maps.Marker({
+        position: latlng,
+        map: map,
+        title: 'Hey buddy.'
+    });
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
