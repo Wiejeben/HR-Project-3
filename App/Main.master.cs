0c011b7e95835997104cbad45c189fa8b5a4923f
Maarten de Graaf <wiejeben@gmail.com>
2016-04-08 14:23:15 +0200

    Add MySQL init script

--- App/Main.master.cs
+++ App/Main.master.cs
@@ -4,11 +4,14 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using MySql.Data.MySqlClient;
 
 public partial class Main : System.Web.UI.MasterPage
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        // Init database
+        // MySqlConnection conn = new MySqlConnection("server=127.0.0.1;uid=root;" + "pwd=root;database=project3;");
+        // conn.Open();
     }
 }
