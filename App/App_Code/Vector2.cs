4041443f522e6208ce62c527b3b1f27ba3870363
Laurens Vastenhout <laurens97@live.nl>
2016-04-12 11:37:08 +0200

    reverted vector2

--- App/App_Code/Vector2.cs
+++ App/App_Code/Vector2.cs
@@ -5,12 +5,22 @@ using System.Web;
 
 public class Vector2
 {
-    public double X { get; }
-    public double Y { get; }
+    double _x;
+    double _y;
 
     public Vector2(double posx, double posy)
     {
-        this.X = posx;
-        this.Y = posy;
+        this._x = posx;
+        this._y = posy;
+    }
+
+    public double X()
+    {
+        return this._x;
+    }
+
+    public double Y()
+    {
+        return this._y;
     }
 }
