6cc761c712e7d355f777b40f08e82a0a7cf1a343
Jason Sjambar <realjase@gmail.com>
2016-04-12 11:29:09 +0200

    Autocompletion in progress.

--- App/Search.aspx.cs
+++ App/Search.aspx.cs
@@ -2,8 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Services;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Web.Script.Serialization;
+
 
 public partial class Search : System.Web.UI.Page
 {
@@ -26,5 +29,25 @@ public partial class Search : System.Web.UI.Page
             // Redirect implementation if no search string was entered
             // Server.Transfer("Default.aspx", true);
         }
+    }
+
+    [WebMethod]
+    public static string Find(string query)
+    {
+        // Get all the results with the entered query.
+        List<Street> results = Street.Find(query);
+
+        List<string> streetNames = new List<string>();
+        // We only want the names, so we place this in a string list.
+        foreach(Street street in results)
+        {
+            streetNames.Add(street.Name);
+        }
+        // Json serializer to convert the names into a json string
+        var jsonSerializer = new JavaScriptSerializer();
+        var jsonNames = jsonSerializer.Serialize(streetNames);
+
+        // Return the json string to the ajax call
+        return jsonNames;
     }
 }
--- App/assets/js/autocompletion.js
+++ App/assets/js/autocompletion.js
@@ -1,5 +1,27 @@
 ﻿$("#MainContent_search_ac").on("input", function () {
+    // Input variables
     var searchf = $("#MainContent_search_ac");
+    var search_charcount = $(searchf).val().replace(/ /g, '').length;
+    var search_query = $(searchf).val();
 
+    // Variables for the ajax call
+    var url = "Search.aspx/Find";
+    var data = '{ "query": "'+search_query+'" }';
 
+    
+    if (search_charcount >= 3) {
+        $.ajax({
+            type: "POST",
+            url: url,
+            data: data,
+            dataType: 'json',
+            contentType: 'application/json',
+            success: function (data) {
+                console.log(data);
+            },
+            error: function (data, status) {
+                alert("Error.");
+            }
+        });
+    }
 });
