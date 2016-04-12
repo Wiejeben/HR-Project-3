75a000b6fac53198dbf51c8be047ecbfef0b7072
Laurens Vastenhout <laurens97@live.nl>
2016-04-12 11:28:18 +0200

    edit get distance and vector 2 change

--- App/App_Code/Actions.cs
+++ App/App_Code/Actions.cs
@@ -28,4 +28,24 @@ public class Actions
 
         return output;
     }
+    public static double getDistance(Vector2 pos1, Vector2 pos2)
+    {
+        var sLatitudeRadians = pos1.X * (Math.PI / 180.0);
+        var sLongitudeRadians = pos1.Y * (Math.PI / 180.0);
+        var eLatitudeRadians = pos2.X * (Math.PI / 180.0);
+        var eLongitudeRadians = pos2.Y * (Math.PI / 180.0);
+
+        var dLongitude = eLongitudeRadians - sLongitudeRadians;
+        var dLatitude = eLatitudeRadians - sLatitudeRadians;
+
+        var result1 = Math.Pow(Math.Sin(dLatitude / 2.0), 2.0) +
+                      Math.Cos(sLatitudeRadians) * Math.Cos(eLatitudeRadians) *
+                      Math.Pow(Math.Sin(dLongitude / 2.0), 2.0);
+
+        // Using 3956 as the number of miles around the earth
+        var result2 = 3956.0 * 2.0 *
+                      Math.Atan2(Math.Sqrt(result1), Math.Sqrt(1.0 - result1));
+
+        return result2;
+    }
 }
