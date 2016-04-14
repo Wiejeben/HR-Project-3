8bd2a4c282f4f812d3f884b6dd30e32e1f3c0888
Maarten de Graaf <maarten@webtraders.nl>
2016-04-14 15:23:08 +0200

    Implement search auto complete url

--- App/Search_Ajax.aspx.cs
+++ App/Search_Ajax.aspx.cs
@@ -6,6 +6,18 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Web.Script.Serialization;
 
+public class Search_Result_Street
+{
+    public int ID;
+    public string Name;
+
+    public Search_Result_Street(int id, string name)
+    {
+        this.ID = id;
+        this.Name = name;
+    }
+}
+
 public partial class Search_Ajax : System.Web.UI.Page
 {
     protected string JSONResponse;
@@ -15,12 +27,14 @@ public partial class Search_Ajax : System.Web.UI.Page
         if (Request.QueryString["q"] != null)
         {
             string query = Request.QueryString["q"];
-            List<string> streets = new List<string>();
+            List<Search_Result_Street> streets = new List<Search_Result_Street>();
 
             // Only give names
             foreach (Street street in Street.Find(query, 10))
             {
-                streets.Add(street.Name);
+                Search_Result_Street result = new Search_Result_Street(street.ID, street.Name);
+
+                streets.Add(result);
             }
 
             // JSON parser
