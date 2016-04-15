2120ef0dc69f1c7e9d1a450124e463971da567b9
Maarten de Graaf <maarten@webtraders.nl>
2016-04-16 00:06:11 +0200

    Optimise search

--- App/Search_Ajax.aspx.cs
+++ App/Search_Ajax.aspx.cs
@@ -6,18 +6,6 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Web.Script.Serialization;
 
-public class Search_Result_Street
-{
-    public int ID;
-    public string Name;
-
-    public Search_Result_Street(int id, string name)
-    {
-        this.ID = id;
-        this.Name = name;
-    }
-}
-
 public partial class Search_Ajax : System.Web.UI.Page
 {
     protected string JSONResponse;
@@ -27,12 +15,12 @@ public partial class Search_Ajax : System.Web.UI.Page
         if (Request.QueryString["q"] != null)
         {
             string query = Request.QueryString["q"];
-            List<Search_Result_Street> streets = new List<Search_Result_Street>();
+            List<dynamic> streets = new List<dynamic>();
 
             // Only give names
             foreach (Street street in Street.Find(query, 10))
             {
-                Search_Result_Street result = new Search_Result_Street(street.ID, street.Name);
+                dynamic result = new { ID = street.ID, Name = street.Name };
 
                 streets.Add(result);
             }
